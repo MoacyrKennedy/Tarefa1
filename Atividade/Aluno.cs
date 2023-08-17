71fb14c32125200c1d52314577b2e1c58547e00b
Moacyr kennedy <105392455+MoacyrKennedy@users.noreply.github.com>
2023-08-16 22:21:55 -0300

    Tarefa Faculdade

---
+++ Atividade/Aluno.cs
@@ -0,0 +1,52 @@
+﻿using System.Globalization;
+using System.Net.Mime;
+
+namespace Atividade;
+
+public class Aluno
+{
+    //Atributos
+    public double nota1, nota2, nota3, nota4;
+    
+    
+    //Cálculo da Média
+    public double Media()
+    {
+        return (nota1 + nota2 + nota3 + nota4) / 4;
+    }
+    //Situação
+    public string Avaliacao(double media)
+    {
+        String retorno;
+        //Condicional
+        if(media <4.9)
+        {
+            retorno = "Infelizmente você foi reprovado";
+        }
+
+         if (media >= 5 && media <= 6.9)
+        {
+            retorno = "Você está em Recuperação";
+        }
+        else 
+        {
+            retorno = "Parabéns você foi Aprovado!";
+        }
+        return retorno;
+    }
+    public void mensagem()
+    {
+        //Obter o cálculo
+        double obterCalculo = Media();
+
+        //Obter situação
+        string obterSituacao = Avaliacao(obterCalculo);
+
+        //Exibir mensagem
+        Console.WriteLine("Sua média "+obterCalculo);
+        Console.WriteLine("Sua média"+obterSituacao);
+        Console.WriteLine("Sua média "+obterCalculo);
+        Console.WriteLine("Sua média "+obterCalculo);
+        Console.WriteLine("Sua média "+obterCalculo);
+    }
+}
---
+++ Atividade/Program.cs
@@ -0,0 +1,46 @@
+﻿using Atividade;
+using System;
+using static Atividade.Aluno;
+
+
+//Instânciar Aluno
+
+// Aluno a = new Aluno();
+// a.nota1 = 7;
+// a.nota2 = 6;
+// a.nota3 = 5;
+// a.nota4 =  7.5;
+
+using System.Globalization;
+using System.Net.Mime;
+
+
+
+using System;
+
+namespace Atividade
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            for (int i = 1; i <= 10; i++)
+            {
+                Console.WriteLine($"Entre com as notas do Aluno {i}:");
+                double nota1 = Convert.ToDouble(Console.ReadLine());
+                double nota2 = Convert.ToDouble(Console.ReadLine());
+                double nota3 = Convert.ToDouble(Console.ReadLine());
+                double nota4 = Convert.ToDouble(Console.ReadLine());
+
+                Aluno aluno = new Aluno();
+                aluno.nota1 = nota1;
+                aluno.nota2 = nota2;
+                aluno.nota3 = nota3;
+                aluno.nota4 = nota4;
+
+                aluno.mensagem();
+                Console.WriteLine();
+            }
+        }
+    }
+}
